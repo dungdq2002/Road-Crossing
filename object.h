a750f4912f9bdbc131154cafeba86ce8be27d612
viethoang1211 <viethoang12112002@gmail.com>
2021-12-08 21:28:02 +0700

    Update object.h

--- object.h
+++ object.h
@@ -43,9 +43,9 @@ public:
 	virtual float getY() = 0;
 	virtual int getWidth() = 0;
 	virtual int getHeight() = 0;
-	virtual void moveHorizontal(int sX, int sY, bool toRight, sf::Vector2u windowSize) {
+	/*virtual void moveHorizontal(int sX, int sY, bool toRight, sf::Vector2u windowSize) {
 
-	}
+	}*/
 	virtual void render(sf::RenderWindow& l_window) = 0;
 };
 
